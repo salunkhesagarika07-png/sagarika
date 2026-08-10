bc68a54fbce4146b8557dc3aca0875c48592a20c
salunkhesagarika07-png <salunkhesagarika07@gmail.com>
2026-08-10 14:07:48 +0000

    area circ.cpp

---
+++ circ.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius, area, circumference;
+    const float PI = 3.14;
+
+    cout << "Enter radius: ";
+    cin >> radius;
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    cout << "Area = " << area;
+    cout << "\nCircumference = " << circumference;
+
+    return 0;
+}
